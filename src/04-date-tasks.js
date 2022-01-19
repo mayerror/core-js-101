66ed7c64a84c9696052c181bcf5589efc7410f63
Sergey Masiuk <mayerror@gmail.com>
2022-01-19 16:54:30 +0300

    feat: implement a few date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,9 +53,20 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  if (!(date.getFullYear() % 4)) {
+    if (!(date.getFullYear() % 100)) {
+      if (!(date.getFullYear() % 400)) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
 }
+
+// isLeapYear(new Date(2000, 1, 1));
 
 
 /**
@@ -73,9 +84,24 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const zeroAdding = (number, digits) => {
+    if (digits === 2) {
+      return number < 10 ? `0${number}` : `${number}`;
+    }
+    return number < (10 ** (digits - 1)) ? `0${zeroAdding(number, digits - 1)}` : `${zeroAdding(number, digits - 1)}`;
+  };
+
+  const diff = +endDate - +startDate;
+  const ms = diff % 1000;
+  const s = Math.floor(diff / 1000) % 60;
+  const min = Math.floor(diff / (1000 * 60)) % 60;
+  const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+
+  return `${zeroAdding(hours, 2)}:${zeroAdding(min, 2)}:${zeroAdding(s, 2)}.${zeroAdding(ms, 3)}`;
 }
+
+// console.log(timeSpanToString(new Date(2000, 1, 1, 10, 0, 0), new Date(2000, 1, 1, 10, 0, 6)));
 
 
 /**
